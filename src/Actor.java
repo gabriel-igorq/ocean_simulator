fbef68c2c0164a4b406062dc333e3269083371c4
Gabriel Igor Queiroz Costa <gabriel.igorq@gmail.com>
2018-10-12 20:20:13 -0300

    Adicionando tags em Actors

--- src/Actor.java
+++ src/Actor.java
@@ -1,13 +1,22 @@
 import java.util.List;
 
+/**
+ * Uma interface contendo todas as classes que possuem um método act
+ * 
+ * @author Gabriel Igor Queiroz Costa, Victor Hugo Freire Ramalho
+ * @version 12/10/2018
+ */
+
 public interface Actor {
 
 	/**
-	 * Executes regular actions from actor.
-	 * @param newActors A list to storage new actors created.
+	 * Ações comuns dos atores
+	 * @param newActors Uma lista para armazenar os atores.
 	 */
-	
 	void act(List <Actor> newActors);
 	
+	/**
+	 * @return true se o ator está vivo e falso se não estiver.
+	 */
 	boolean isAlive();
 }
